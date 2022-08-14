67a84d6ce0c5f865e80bf94e6542fb734db04529
Aniket525 <aniketkarp524@gmail.com>
2022-08-14 20:40:13 +0530

    first commit

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 import mongoose from'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 
 const connection = async() => {
     try {
-       await mongoose.connect("mongodb+srv://Aniket524:pass123@cluster0.iztsmlu.mongodb.net/user",{useNewUrlParser:true})
+       await mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true})
        console.log("database connected successfully");
     } catch (error) {
         console.error("error whle coonecting with the database",error);
